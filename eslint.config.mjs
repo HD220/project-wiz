b69df55e4c2ede3ee9edce82c3807c93a7c4d09d
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2024-10-04 21:37:04 -0300

    feat: add eslint typescript on staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    rules: {
-      "no-unused-vars": "error",
-      "no-undef": "error",
-    },
-    // parser: "@typescript-eslint/parser",
-    // plugins: ["@typescript-eslint"],
-  },
-  { languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
