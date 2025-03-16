d8d9bfe9b389904f800c2ab14ea0d1b0abb1cb53
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2025-03-16 17:47:21 -0300

    Adicionado shadcnui

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" className="">
           {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
         </a>
         <a href="https://react.dev" target="_blank">
@@ -14,10 +15,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div>
+        <Button
+          variant={"default"}
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
