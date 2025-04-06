d8e0990998f504960f9579d3f1afe95ffd10f20f
Nicolas Fraga Faust <nicolas.faust@wmw.com.br>
2025-04-06 09:34:29 -0300

    chore: remove sistema de plugins e exemplos

--- src/core/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-export { PluginHost } from "./host/plugin-host.js";
-export { ServiceRegistry } from "./registry/service-registry.js";
-export type { Plugin, PluginConfig } from "./types.js";
-
-// Re-export common types for plugin developers
-export * from "./types.js";
