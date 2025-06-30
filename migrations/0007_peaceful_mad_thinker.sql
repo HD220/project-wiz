823f9c2b8663a204cd4c4cc945ba4fbdece339fb
Nicolas Fraga Faust <nicolas.faust@hotmail.com>
2025-06-30 19:36:30 -0300

    realizada algumas limpezas

--- migrations/0007_peaceful_mad_thinker.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `queues` (
-	`id` text PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`concurrency` integer DEFAULT 1 NOT NULL,
-	`created_at` integer DEFAULT '"2025-06-16T14:53:55.970Z"' NOT NULL,
-	`updated_at` integer DEFAULT '"2025-06-16T14:53:55.970Z"' NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `queues_name_unique` ON `queues` (`name`);
